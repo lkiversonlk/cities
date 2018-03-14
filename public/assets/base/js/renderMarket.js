951e5aacca043e87089cf1057dc70ed407d21e2b
liukan <liukan@bytedance.com>
2018-03-14 18:42:27 +0800

    auto
    Change-Id: I7f816b399ad3ff63210b9b240b52b56eef45352a

--- public/assets/base/js/renderMarket.js
+++ public/assets/base/js/renderMarket.js
@@ -40,13 +40,12 @@ ether
 function render() {
   $('#grid-container [name="price"]').each((i, ele) => {
     let id = ele.getAttribute('e-id');
-    //console.log(id);
     ether.getPriceWithFloor(id)
     .then(price => {
       $(ele).text(`${price} ETH`);
     })
   });
-
+  
   $('#grid-container [name="reproducible"]').each((i, ele) => {
     let id = ele.getAttribute('e-id');
     ether.reproducable(id)
@@ -57,7 +56,15 @@ function render() {
         $(ele).text('Unreproducible');
       }
     })
-  })
+  });
+
+  $('#grid-container [name="action"]').each((i, ele) => {
+    let id = ele.getAttribute('e-id');
+    ether.stage(id)
+    .then(result => {
+      $(ele).text(result)
+    })
+  });
 }
 
 render();
