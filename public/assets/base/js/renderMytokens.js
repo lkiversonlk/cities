04239b45f0e08f3731ee292f670191d5fb91f09a
liukan <liukan@bytedance.com>
2018-03-15 00:34:09 +0800

    auto
    Change-Id: I732844ab21f2997c3ee3ddb9c3ef2a5fe19932ba

--- public/assets/base/js/renderMytokens.js
+++ public/assets/base/js/renderMytokens.js
@@ -1,3 +1,18 @@
+let url = window.location.href;
+
+let ether = ethereum((err) => {
+  alert(err);
+})
+
+if(url.slice(-8).indexOf("tokens") != -1) {
+
+  //redirect
+  ether.getWeb3.then(ethereum => {
+    let addr = ethereum.address;
+    window.location.href = `/mytokens/${addr}`; //relative to domain
+  })
+}
+
 function render() {
   $('#grid-container [name="price"]').each((i, ele) => {
     let id = ele.getAttribute('e-id');
