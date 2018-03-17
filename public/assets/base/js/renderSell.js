43b6aa1ca6adae509b87c64d4ba07a5475023d6c
liukan <liukan@bytedance.com>
2018-03-17 17:38:52 +0800

    auto
    Change-Id: I35b1abdf3946b2bc74e30984f20d7d39fbc3c8b9

---
+++ public/assets/base/js/renderSell.js
@@ -0,0 +1,58 @@
+ether
+.getWeb3
+.then(ethereum =>{
+  return ether.config
+  .then(() => {
+    $('#user').text(`Wallet: ${ethereum.address.slice(0,10)}...`)
+  })
+})
+.catch((err) => {
+  switch(err) {
+    case 'WEB3MISS':
+      $('#login-form').modal('show')
+      break
+    case 'NOACCOUNT':
+      $('#locked-form').modal('show')
+      break
+    case 'NETWORKNOTSUPPORTED':
+      $('#user').text(`wrong network`)
+      alert('wrong network')
+  }
+})
+
+
+let ele = $('#sell')[0]
+
+if(ele) {
+  let id = ele.getAttribute('e-id');
+
+  ether.getContractIns
+  .then(ins => {
+    return new Promise((resolve, reject) => {
+      ins.positions(id, (err, pos) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(pos[1].toNumber())
+        }
+      })
+    })
+  })
+  .then(price => {
+    return ether.getWeb3
+    .then(ethereum => {
+      let high = ethereum.web3.fromWei(price * 150 / 100)
+      let low = ethereum.web3.fromWei(price)
+      $('#high').val(high)
+      $('#low').val(low)
+    })
+  })
+
+  $("#sell").click(() => {
+    ether.sellToken(id)
+    .then(tx => {
+      console.log(tx)
+    })
+  })
+}
+
